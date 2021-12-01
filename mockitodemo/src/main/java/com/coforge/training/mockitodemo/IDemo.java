ebe9a78aa3c93e403b18a6d6dbc6db7a4e5fc9f5
Anushka Saxena <anushka.saxena411@gmail.com>
2021-12-01 09:29:53 +0530

    first commit

---
+++ mockitodemo/src/main/java/com/coforge/training/mockitodemo/IDemo.java
@@ -0,0 +1,10 @@
+package com.coforge.training.mockitodemo;
+
+public interface IDemo {
+	
+	String S="Hello World"; // by default its final or a constant
+	String greet(); //by default abstract
+	
+	
+
+}
